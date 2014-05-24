5a32470c4490690ec4c9db042b44d9d0bcbd79d2
Whitebox <sanddudu@gmail.com>
2014-05-24 23:31:21 +0800

    0.0.3: fix style and hitokoto

--- static/javascripts/hitokoto.js
+++ static/javascripts/hitokoto.js
@@ -1,33 +1,38 @@
-function hitokoto () {
-	function getcat () {
-		var cat;
-		var d = new Date();
-		var day = d.getDate();
-		if (day % 2 == 0) {
-			cat = "a";
-		} else if (day % 3 == 0) {
-			cat = "b";
-		} else if (day % 4 == 0) {
-			cat = "c";
-		} else {
-			cat = "d";
+$(document).ready(function () {
+	function hitokoto () {
+		function getcat () {
+			var cat;
+			var d = new Date();
+			var day = d.getDate();
+			if (day % 2 == 0) {
+				cat = "a";
+			} else if (day % 3 == 0) {
+				cat = "b";
+			} else if (day % 4 == 0) {
+				cat = "c";
+			} else {
+				cat = "d";
+			}
+			return cat;
 		}
-		return cat;
+		$.ajax({
+			type : "get",
+			async: false,
+			url : "http://api.hitokoto.us/rand?encode=jsc&length=20&cat=" + getcat(),
+			dataType : "jsonp",
+			jsonp: "fun",
+			jsonpCallback:"hitokoto",
+			success : function(json){
+				var html = json.hitokoto;
+					html += "<span class='desc'>";
+					html += "——" + " " + "《" + json.source + "》";
+					html += "</span>";
+					$("#hitokoto").html(html);
+				},
+			error : function (e) {
+				console.log("Get Hitokoto Faied!");
+			}
+		});
 	}
-	$.ajax({  
-		type : "get",
-		url : "http://api.hitokoto.us/rand?length=20&cat=" + getcat(),
-		dataType : "json",
-		success : function(json){
-			var html = json.hitokoto;
-				html += "<span class='desc'>";
-				html += "——" + " " + "《" + json.source + "》";
-				html += "</span>";
-				$("#hitokoto").html(html);
-			},
-		error : function (e) {
-			console.log("Get Hitokoto Faied!");
-		}
-	});
-}
-hitokoto();
+	hitokoto();
+});
